36579d0d942429808d24cc938cd71f10dd903ddc
Alexander Krechetov <Ko4kaNto4kA@yandex.ru>
2024-10-01 21:32:59 +0300

    add_task

--- main.py
+++ main.py
@@ -1,28 +1,34 @@
+from datetime import datetime
+
+
 tasks = []
+num_id = 0
 
 
 def show_tasks():
+    """Выводит в консоль задачи."""
     if tasks:
-        for name, status in tasks:
-            print(name, status)
+        for task in tasks:
+            print(task)
     else:
         print('У вас нет задач')
 
 
 def add_task():
-    print('Давай добавим новую задачу!')
-    title = input('Введи название задачи. Поле не может быть пустым.')
-    description = input('Можешь добавить описание задачи, но это необязательно)')
-    status = int(input(
-        '''Выбери цифру из перечня ниже для присвоения задаче статуса.
-        1 - "новая" (задача только что создана и ещё не начата);
-        2 - "в процессе" (задача в процессе выполнения);
-        3 - "завершена" (задача выполнена);
-        4 - "отменена" (задача была отменена и больше не актуальна)
-    '''))
-    priority = int(input('Установи приоритет задачи. 1 — высокий, 2 — средний, 3 — низкий. Обязательное поле.'))
-    due_date = None # дата
-    tasks.append({name: False})
+    """Добавляет новую задачу в список задач."""
+    print('Добавим новую задачу!')
+    title = input('Введите название задачи. Поле не может быть пустым.')
+    description = input('Можем добавить описание задачи, но это необязательно)')
+    priority = int(input('Необходимо установить приоритет задачи. 1 — высокий, 2 — средний, 3 — низкий. Обязательное поле.'))
+    time_stamp = datetime.now()
+    due_date = datetime.strptime(input('Введите дату и время дедлайна в формате dd.mm.yyyy hh:mm'), '%d.%m.%Y %H:%M')
+    num_id += 1
+    tasks.append({'id': num_id, 'title': title, 'description': description, 'priority': priority, 'time_stamp': time_stamp, 'due_time': due_date})
+
+
+def edit_task():
+    """Изменяет название, описание, приоритет или срок дедлайна задачи."""
+    pass
 
 
 def delete_task():
@@ -46,25 +52,33 @@ def complete_task():
 
 
 def main():
+    """Основная логика программы."""
     while True:
         print('To-Do List')
-        print('1. Показать все задачи')
+        print()
+        print('Возможные действия')
+        print()
+        print('1. Показать задачи')
         print('2. Добавить задачу')
-        print('3. Удалить задачу')
-        print('4. Отметить задачу как выполненную')
-        print('5. Выйти')
+        print('3. Изменить задачу')
+        print('4. Удалить задачу')
+        print('5. Отметить задачу как выполненную')
+        print('6. Выйти')
+        print()
         
-        num = int(input('Выберите действие (1-5)'))
+        num = int(input('Выберите действие (1-6)'))
 
         if num == 1:
             show_tasks()
         elif num == 2:
             add_task()
         elif num == 3:
-            delete_task()
+            edit_task()
         elif num == 4:
-            complete_task()
+            delete_task()
         elif num == 5:
+            complete_task()
+        elif num == 6:
             print('Завершение работы')
             break
         else:
